22b67b8f215374b3965604ec977de830a0fb5b2c
Bobby Towers <porkostomus@mail.com>
2022-03-02 12:49:06 -0800

    add views, routes and controllers

--- app.cljs
+++ app.cljs
@@ -1,26 +1,24 @@
 (ns app
   (:require ["express$default" :as express]
             ["path" :as path]
-            ["mongoose$default" :as mongoose]))
+            ["mongoose$default" :as mongoose]
+            ["./routes/catalog$default" :as catalogRouter]))
 
 (def app (express))
 (def port 3000)
 
 (def mongoDB "mongodb://127.0.0.1/my_database")
-
 (.connect mongoose mongoDB
  #js {:useNewUrlParser true, :useUnifiedTopology true})
 
 (def db (.-connection mongoose))
-
 (.on db "error" (.bind js/console.error "MongoDB connection error:"))
 
 (.set app "view engine" "pug")
 (.use app (.static express (.join path (.cwd js/process) "public")))
 
-(.get app "/"
-      (fn [req res next]
-        (.render res "index" #js {:title "nbb-express"})))
+(.use app "/catalog" catalogRouter)
+(.get app "/" (fn [req res] (.redirect res "/catalog")))
 
 (.get app "/users"
       (fn [req res next]
